c9bf4b91f5e97d0e1b00b3cb21f8a9e0d321e2aa
GabrielDiasGomesProgramer <161972832+GabrielDiasGomesProgramer@users.noreply.github.com>
2024-09-29 18:25:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from './componentes/navbar/Navbar'
+import './App.css'
+import logotipo from './assets/logo.png'
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <img src={logotipo} alt="" srcset="" />
+    </>
+  )
+}
+
+export default App
